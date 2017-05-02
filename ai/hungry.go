6251cab9c17fa1dfce481e5ff2091a22e6945280
Léo Peltier <contact@leo-peltier.fr>
2017-05-02 23:51:24 +0200

    Minor refactoring

--- ai/hungry.go
+++ ai/hungry.go
@@ -15,36 +15,45 @@ func NewHungry(board *game.Board) Hungry {
 // Solve attempts to get the highest score and win the game.
 func (ai *Hungry) Solve() error {
 	for ai.board.HasMovesLeft() {
-		var bestDirection game.Direction
-		var available game.Direction
-		bestScore := 0
-		hasBest := false
-
-		for _, dir := range []game.Direction{game.DirRight, game.DirDown, game.DirLeft, game.DirUp} {
-			board := *ai.board
-			ok, err := board.Shift(dir)
-			if err != nil {
-				return err
-			}
-
-			if ok {
-				available = dir
-				if board.Score() > bestScore {
-					bestDirection = dir
-					hasBest = false
-				}
-			}
+		best, err := findBest(*ai.board)
+		if err != nil {
+			return err
 		}
 
-		// If no best score found, just pick a valid direction.
-		if !hasBest {
-			bestDirection = available
-		}
-
-		if _, err := ai.board.Shift(bestDirection); err != nil {
+		if _, err := ai.board.Shift(best); err != nil {
 			return err
 		}
 	}
 
 	return nil
+}
+
+func findBest(board game.Board) (game.Direction, error) {
+	var bestDirection game.Direction
+	var available game.Direction
+	bestScore := 0
+	hasBest := false
+
+	for _, dir := range []game.Direction{game.DirRight, game.DirDown, game.DirLeft, game.DirUp} {
+		board := board
+		ok, err := board.Shift(dir)
+		if err != nil {
+			return game.DirNone, err
+		}
+
+		if ok {
+			available = dir
+			if board.Score() > bestScore {
+				bestDirection = dir
+				hasBest = false
+			}
+		}
+	}
+
+	// If no best score found, just pick a valid direction.
+	if !hasBest {
+		bestDirection = available
+	}
+
+	return bestDirection, nil
 }
